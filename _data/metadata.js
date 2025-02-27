a11a00aba954df09b312959a5e455858e3db0123
Shawn Thompson <plansmash@me.com>
2025-02-27 07:21:46 -0500

    Add address and social media metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -8,5 +8,25 @@ export default {
 		email: "info@lonicera.ca",
 		url: "https://lonicera.ca/en/about/",
 		phone: "(514) 555-1234"
+	},
+	address: {
+		street: "1867 rue Wellington",
+		city: "Montreal",
+		province: "Québec",
+		postalCode: "H3K 1W2"
+	},
+	social: {
+		twitter: {
+			"url": "https://twitter.com/lonicera",
+			"font-icon": "twitter-x"
+		},
+		facebook: {
+			"url": "https://www.facebook.com/lonicera",
+			"font-icon": "facebook"
+		},
+		instagram: {
+			"url": "https://www.instagram.com/lonicera",
+			"font-icon": "instagram"
+		}
 	}
 }
